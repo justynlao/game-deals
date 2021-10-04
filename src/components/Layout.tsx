546d962db9891c96af9ae2076e7896f97a5f77cd
jlao1201 <54722681+jlao1201@users.noreply.github.com>
2021-10-03 18:11:48 -0700

    updated imports

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
 
-import styles from "../../styles/Layout.module.css";
+import styles from "../styles/Layout.module.css";
 
 interface LayoutProps {
   children: any;
