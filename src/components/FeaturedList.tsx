1d6e34a271c2254c228aab8877aac4bf863f27a0
jlao1201 <54722681+jlao1201@users.noreply.github.com>
2021-10-04 15:29:06 -0700

    added FeaturedList component

--- src/components/FeaturedList.tsx
+++ src/components/FeaturedList.tsx
@@ -1,9 +1,46 @@
-const FeaturedList = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+import { Title } from "@mui/icons-material";
+import { useState } from "react";
+
+import GameCard from "./GameCard";
+
+import styles from '../styles/FeaturedList.module.css'
+
+interface Game {
+  title: string;
+  normalPrice: string;
+  salePrice: string;
+  savings: string;
+  thumb: string;
 }
 
-export default FeaturedList
+interface FeaturedListProps {
+  featuredGames: Game[];
+}
+
+const FeaturedList = ({ featuredGames }: FeaturedListProps) => {
+  return (
+    <div className={styles.featured_container}>
+      {featuredGames.map((game, index) => {
+        const {
+          title: title,
+          normalPrice: normalPrice,
+          salePrice: salePrice,
+          savings: savings,
+          thumb: thumb,
+        }: Game = game;
+        return (
+          <GameCard
+            key={index}
+            title={title}
+            normalPrice={normalPrice}
+            salePrice={salePrice}
+            savings={savings}
+            thumb={thumb}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default FeaturedList;
