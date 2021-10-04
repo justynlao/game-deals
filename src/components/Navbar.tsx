d0d76ee8acd55cacac60146940a0b59f0511d61a
jlao1201 <54722681+jlao1201@users.noreply.github.com>
2021-10-03 18:11:34 -0700

    styled navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,30 @@
 import Link from "next/link";
+import { SportsEsports } from "@mui/icons-material";
 
 import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
-  return <nav className={styles.nav}>
-      
-  </nav>;
+  return (
+    <nav className={styles.nav}>
+      <Link href="/" passHref>
+        <div className={styles.nav_logo}>
+          <SportsEsports color="inherit" fontSize="large" />
+          <div className={styles.nav_logo_label}>Dyno Deals</div>
+        </div>
+      </Link>
+      <div className={styles.nav_links}>
+        <Link href="/" passHref>
+          <div className={styles.nav_link}>Deals</div>
+        </Link>
+        <Link href="/" passHref>
+          <div className={styles.nav_link}>Favorites</div>
+        </Link>
+        <Link href="/" passHref>
+          <div className={styles.nav_link}>Login</div>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
