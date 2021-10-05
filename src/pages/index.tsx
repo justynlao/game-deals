c98313e34def628417fa927bba41cefeb273a675
jlao1201 <54722681+jlao1201@users.noreply.github.com>
2021-10-05 14:03:01 -0700

    changed data fetching to getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,33 @@
-import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import axios from "axios";
 
 import FeaturedList from "../components/FeaturedList/FeaturedList";
 
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
-  const [featuredGames, setFeaturedGames] = useState([]);
-
-  useEffect(() => {
-    getFeaturedData();
-  }, []);
-
-  const getFeaturedData = async () => {
-    const resp = await axios.get(
-      "https://www.cheapshark.com/api/1.0/deals?storeID=1,3,7,11"
-    );
-    setFeaturedGames([...resp.data]);
-  };
-
+const Home: NextPage = ({
+  games,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <main className={styles.main}>
-      <h1 className={styles.heading}>Find the best deals on PC games!</h1>
-      <FeaturedList featuredGames={featuredGames} />
+      <h1 className={styles.heading}>Featured Deals</h1>
+      <FeaturedList featuredGames={games} />
     </main>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await axios.get(
+    "https://www.cheapshark.com/api/1.0/deals?storeID=1,3,7,11"
+  );
+  const games = res.data;
+
+  return {
+    props: {
+      games,
+    },
+    revalidate: 60 * 60 * 24,
+  };
 };
 
 export default Home;
