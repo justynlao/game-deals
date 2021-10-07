64e856d334702cb744db5bdbc7a3a5b32f14ab6c
jlao1201 <54722681+jlao1201@users.noreply.github.com>
2021-10-06 22:37:50 -0700

    added store detail props and basic page layout

--- src/pages/game/[id].tsx
+++ src/pages/game/[id].tsx
@@ -1,17 +1,26 @@
+/* Component/Function Imports */
+import GamePriceInfo from "../../components/GameInfo/GameInfo";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+/* Library Imports */
 import axios from "axios";
+
+/* Type Imports */
 import {
   NextPage,
   GetStaticProps,
   GetStaticPaths,
   InferGetStaticPropsType,
 } from "next";
-import { useRouter } from "next/router";
-import { DealInfo } from "../../types/deal-info";
+import { DealInfoType } from "../../types/deal-info";
 
+/* Style Imports */
 import styles from "../../styles/GameDetails.module.css";
 
 const GameDetails: NextPage = ({
   gameInfo,
+  storeInfo,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const router = useRouter();
 
@@ -19,10 +28,14 @@ const GameDetails: NextPage = ({
     return <h1> Loading...</h1>;
   }
 
+  const { info, cheapestPriceEver, deals } = gameInfo;
+  const img_url = `https://cdn.cloudflare.steamstatic.com/steam/apps/${info.steamAppID}/header.jpg`;
+
   return (
     <main className={styles.main}>
       <h1 className={styles.heading}>{gameInfo.info.title}</h1>
-      <p>{gameInfo.info.title}</p>
+      <Image src={img_url} alt="game banner" width="460" height="215" />
+      <GamePriceInfo cheapestPrice={cheapestPriceEver} deals={deals} storeDetails={storeInfo} />
     </main>
   );
 };
@@ -34,9 +47,13 @@ export const getStaticProps: GetStaticProps = async (context) => {
   );
   const gameInfo = res.data;
 
+  const res2 = await axios.get("https://www.cheapshark.com/api/1.0/stores");
+  const storeInfo = res2.data;
+
   return {
     props: {
       gameInfo,
+      storeInfo,
     },
   };
 };
@@ -45,9 +62,9 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const res = await axios.get(
     "https://www.cheapshark.com/api/1.0/deals?storeID=1,3,7,11"
   );
-  const deals: DealInfo[] = res.data;
+  const deals: DealInfoType[] = res.data;
 
-  const paths = deals.map((deal: DealInfo) => {
+  const paths = deals.map((deal: DealInfoType) => {
     return {
       params: { id: deal.gameID.toString() },
     };
