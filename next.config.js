2777418badf8d89b7c1b7f2a07e0e98cd4663908
jlao1201 <54722681+jlao1201@users.noreply.github.com>
2021-10-06 22:38:39 -0700

    added image source domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["cdn.cloudflare.steamstatic.com", "www.cheapshark.com"],
+  },
+};
