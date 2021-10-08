d4a070aff4b1cb791fa5d82a8ad82b1a0f98ef63
jlao1201 <54722681+jlao1201@users.noreply.github.com>
2021-10-07 21:31:21 -0700

    added additional data to GameDetails page

--- src/pages/game/[id].tsx
+++ src/pages/game/[id].tsx
@@ -1,18 +1,15 @@
 /* Component/Function Imports */
-import GamePriceInfo from "../../components/GameInfo/GameInfo";
-import Image from "next/image";
+import GamePriceList from "../../components/GamePriceList/GamePriceList";
+import AdditionalDetails from "../../components/AdditionalDetails/AdditionalDetails";
 import { useRouter } from "next/router";
 
 /* Library Imports */
 import axios from "axios";
 
 /* Type Imports */
-import {
-  NextPage,
-  GetStaticProps,
-  GetStaticPaths,
-  InferGetStaticPropsType,
-} from "next";
+import { NextPage, GetStaticProps, GetStaticPaths, InferGetStaticPropsType } from "next";
+import { GameInfoType } from "../../types/game-info";
+import { StoreInfoType } from "../../types/store-info";
 import { DealInfoType } from "../../types/deal-info";
 
 /* Style Imports */
@@ -21,6 +18,7 @@ import styles from "../../styles/GameDetails.module.css";
 const GameDetails: NextPage = ({
   gameInfo,
   storeInfo,
+  dealInfo,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const router = useRouter();
 
@@ -28,40 +26,39 @@ const GameDetails: NextPage = ({
     return <h1> Loading...</h1>;
   }
 
-  const { info, cheapestPriceEver, deals } = gameInfo;
-  const img_url = `https://cdn.cloudflare.steamstatic.com/steam/apps/${info.steamAppID}/header.jpg`;
+  const { cheapestPriceEver, deals } = gameInfo;
 
   return (
     <main className={styles.main}>
-      <h1 className={styles.heading}>{gameInfo.info.title}</h1>
-      <Image src={img_url} alt="game banner" width="460" height="215" />
-      <GamePriceInfo cheapestPrice={cheapestPriceEver} deals={deals} storeDetails={storeInfo} />
+      <AdditionalDetails dealInfo={dealInfo} cheapestPrice={cheapestPriceEver} />
+      <GamePriceList deals={deals} storeDetails={storeInfo} />
     </main>
   );
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const id = context?.params?.id;
-  const res = await axios.get(
-    `https://www.cheapshark.com/api/1.0/games?id=${id}`
-  );
-  const gameInfo = res.data;
+  const res = await axios.get(`https://www.cheapshark.com/api/1.0/games?id=${id}`);
+  const gameInfo: GameInfoType = res.data;
 
   const res2 = await axios.get("https://www.cheapshark.com/api/1.0/stores");
-  const storeInfo = res2.data;
+  const storeInfo: StoreInfoType = res2.data;
+
+  const steamID = gameInfo.info.steamAppID;
+  const res3 = await axios.get(`https://www.cheapshark.com/api/1.0/deals?steamAppID=${steamID}`);
+  const dealInfo: DealInfoType[] = res3.data;
 
   return {
     props: {
       gameInfo,
       storeInfo,
+      dealInfo,
     },
   };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const res = await axios.get(
-    "https://www.cheapshark.com/api/1.0/deals?storeID=1,3,7,11"
-  );
+  const res = await axios.get("https://www.cheapshark.com/api/1.0/deals?storeID=1");
   const deals: DealInfoType[] = res.data;
 
   const paths = deals.map((deal: DealInfoType) => {
